2386e4eb68621a65b3877dfe5d8827f2f722cb7b
wanderson almeida <https://github.com/WandersonAlmeida/tabuada>
2022-03-26 11:40:26 -0300

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import { Center } from '@chakra-ui/react'
+import { Text } from '@chakra-ui/react'
+
+
+function Footer(){
+    return(
+        <Center bg='Black' h='170px' color='white'>
+            <Text fontSize='3em'>Awari</Text>
+        </Center>
+    )
+}
+export default Footer
